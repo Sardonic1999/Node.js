21331de5aa1d35934c07aa33001e3f9095a4f71e
Sardonic1999 <sardorjurayev578@gmail.com>
2021-06-19 16:27:33 +0500

    node.js

---
+++ node.js
@@ -0,0 +1,62 @@
+console.log("IT academy")
+
+var a=4
+var b=20
+
+console.log("1-misol")
+console.log(a*b)
+
+console.log("2-misol")
+console.log(7**2)
+
+console.log("3-misol")
+console.log(5*9)
+console.log(2*(10+20))
+console.log("======")
+
+console.log("4-misol")
+var pi=3.14
+var p=10
+console.log(pi*p)
+console.log("======")
+
+console.log("5-misol")
+console.log(6**3)
+console.log(6*3**2)
+console.log("======")
+
+console.log("6-misol")
+console.log(7*5*8)
+console.log(2*(7*5+5*8+7*8))
+console.log("======")
+
+console.log("7-misol")
+console.log(2*3.14*7)
+console.log(3.14*7**2)
+console.log("======")
+
+
+console.log("8-misol")
+console.log((3+4)/2)
+console.log("======")
+
+console.log("9-misol")
+console.log((5*4)**(1/2))
+console.log("======")
+
+
+console.log("10-misoll")
+var a=7
+var b=8
+console.log("a+b,a*b,a*a,b*b")
+console.log(a+b)
+console.log(a*b)
+console.log(a*a)
+console.log(b*b)
+console.log("======")
+
+console.log("12-misol")
+var a=3
+var b=4
+var c=((a**2)+(b**2))**1/2
+console.log(c)
