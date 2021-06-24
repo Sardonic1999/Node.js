09a8234c23764cb976b2a6c166811a813b5e1600
Sardonic1999 <sardorjurayev578@gmail.com>
2021-06-24 22:13:19 +0500

    Switch

---
+++ switch.js
@@ -0,0 +1,16 @@
+// if bilan bir xil yozilishida farq bor
+
+hafta = 0
+
+switch(hafta){
+    case 1: console.log('Dushanba'); break
+    case 2: console.log('Seshanba'); 
+    case 3: console.log('Chorshanba'); break
+    case 4: console.log('Payshanba'); break
+    case 5: console.log('Juma'); break
+    case 6: console.log('Shanba'); break
+    case 7: console.log('Yakshanba'); break
+    default: console.log('bunday hafta kuni mavjud emas')// tepadagi amallar bajarilmasa
+}
+
+// break tormoz vazifasini bajaradi. agar olib tashlansa unadan keyingi brakgacha oladi.
