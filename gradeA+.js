7e210a0a53489962a6cd93c3bbfbea1d490df8e5
Sardonic1999 <sardorjurayev578@gmail.com>
2021-06-24 21:28:52 +0500

    Grade

---
+++ gradeA+.js
@@ -0,0 +1,13 @@
+
+// Grade baholash tizimi.
+grade = 99;
+
+grade > 95 && console.log("A+")
+grade <= 95 && grade >= 90 && console.log("A")
+grade < 90 && grade > 85 && console.log("B+")
+grade < 85 && grade > 80 && console.log("B")
+grade < 80 && grade > 75 && console.log("C+")
+grade < 75 && grade > 70 && console.log("C")
+grade < 70 && grade > 65 && console.log("D+")
+grade < 70 && grade > 65 && console.log("D")
+grade < 65 && grade > 0 && console.log("E")
