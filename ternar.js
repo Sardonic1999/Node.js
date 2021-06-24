0fafc6c2eda01aa4ab0201dcac050b27782a656c
Sardonic1999 <sardorjurayev578@gmail.com>
2021-06-24 22:24:32 +0500

    Ternar

---
+++ ternar.js
@@ -0,0 +1,19 @@
+// so'roq belgisi reactda qollaniladi. if vazifasini bajaradi.
+
+havo = 60
+
+havo > 20 ?
+console.log('havo issiq') // 1-qismi
+: // o'rtasida ajratib turadi
+console.log('havo sovuq') // 2-qism
+
+havo > 20 ? console.log('havo issiq'):console/log('havo sovuq') // bu holatda ham mumkin
+
+havo > 20 ?
+havo < 50 ?
+1
+: console.log('hey')  //true
+: console.log('havo sovuq')  //false
+
+// qachonki false bo'lsa ishlaydi. 
+// ikkta qiymat berilishi shart.
