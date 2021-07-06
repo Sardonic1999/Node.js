06313c71aa09b01d1c595c6ee527401d6a67c7dc
Sardonic1999 <sardorjurayev578@gmail.com>
2021-07-06 15:40:48 +0500

    juft va toq for bilan

---
+++ for.js
@@ -0,0 +1,16 @@
+
+
+let i = 0
+
+// for(i ; i < 10 ; i++){
+//     console.log(i)
+// }
+
+for(i ; i < 11 ; i++){
+    if(i === 0)
+        console.log(i, 'son toq ham juft ham emas')
+    else if(i%2 == 0)
+        console.log(i, 'juft')
+    else
+        console.log(i, "toq son")
+}
