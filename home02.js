86bdbea5f4591ff70f00059e59b78f7f14eb8308
Sardonic1999 <sardorjurayev578@gmail.com>
2021-07-06 15:36:41 +0500

    Matematik amallar

---
+++ home02.js
@@ -0,0 +1,82 @@
+
+
+function pow(a,b){
+    return a**b
+}
+
+console.log(pow(5,2))
+console.log('misol')
+console.log('==================')
+
+// var x = 14.7
+
+// function round(x){
+//     if(x % )
+//     return x
+// }
+// console.log(round(14.7))
+
+console.log('==========================')
+
+function abs(w){
+    if(w>0){
+        return w*1
+    } else if(w<0){
+        return w*(-1)
+    }
+}
+console.log(abs(-84))
+
+console.log('==========================')
+
+function sqrt(f){
+    if(f>0){
+        return f**(1/2)
+    }else if(f<0){
+        return 'uzurli'
+    }
+}
+console.log(sqrt(-64))
+console.log('==========================')
+
+function ceil(y){
+    if((y-parseInt(y)) > 0.1 ){
+        return parseInt(y) + 1
+    }else if((y-parseInt(y)) < 0.1){
+        return parseInt(y)
+    }
+    
+}
+console.log(ceil(6))
+console.log('==========================')
+
+function floor(t){
+    if((t-parseInt(t)) < 1){
+        return parseInt(t)
+    }
+} 
+console.log(floor(5.9))
+
+console.log('==========================')
+
+function round(v){
+    if((v-parseInt(v)) > 0.5){
+        return parseInt(v) +1
+    }else if((v-parseInt(v)) <= 0.5){
+        return parseInt(v)
+    }
+}
+console.log(round(14.6))
+
+console.log('==========================')
+
+let i = 0
+
+function max(l, m, j, u){
+    for(i; i<max.length; i++){
+        return max 
+    }
+}
+console.log(max(2, 4, 6, 9))
+
+// console.log(Math.max(2, 3, 5))
